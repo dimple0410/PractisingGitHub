4aca447120579950978728779673a9aecefbf651
dimple0410 <72303980+dimple0410@users.noreply.github.com>
2020-11-06 16:34:51 +0530

    Update Addition.java

--- GitPrac/src/com/demo/Addition.java
+++ GitPrac/src/com/demo/Addition.java
@@ -7,10 +7,15 @@ public class Addition {
 		int c=a+b;
 		System.out.println(c);
 	}
+	public void sumStr(String s1,String s2)
+	{
+		String s3=s1+s2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Addition a=new Addition();
 		a.sum(10, 20);
+		a.sumStr("A","BCD");
 
 	}
 
