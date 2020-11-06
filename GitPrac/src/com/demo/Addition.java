b6f85abef9fe607c9ba411d9060c0b40a59d1ab6
dimpl <dimpl@LAPTOP-J2EAHDLS>
2020-11-06 16:32:34 +0530

    By Dimple

---
+++ GitPrac/src/com/demo/Addition.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Addition {
+
+	public void sum(int a,int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition a=new Addition();
+		a.sum(10, 20);
+
+	}
+
+}
